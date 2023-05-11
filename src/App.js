f45f53ab9be4bb50c72ab6aa9a3357e6cbde45ce
Reese <arter.charise@gmail.com>
2023-05-11 14:22:27 -0700

    added ReactRouter and Nav setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import categoriesData from './components/categories-data/categories.json';
-import Directory from './components/directory/directory.component';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import { Routes, Route } from 'react-router-dom';
+
+const Shop = () => {
+	return <h1>The Shop</h1>;
+};
 
 const App = () => {
-	const categories = categoriesData;
-	return <Directory categories={categories} />;
+	return (
+		<Routes>
+			<Route path='/' element={<Navigation />}>
+				{/* Siblings - Navigation is on every page, so nest other components here
+				 */}
+				<Route index element={<Home />} />
+				<Route path='/shop' element={<Shop />} />
+			</Route>
+		</Routes>
+	);
 };
 
 export default App;
