95aba2afdbc72550a1e90e0b2e7c5b89e36dc584
Reese <arter.charise@gmail.com>
2023-05-16 10:57:49 -0700

    changed signIn link to camelCase

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
 				 */}
 				<Route index element={<Home />} />
 				<Route path='/shop' element={<Shop />} />
-				<Route path='/sign-in' element={<SignIn />} />
+				<Route path='/signIn' element={<SignIn />} />
 			</Route>
 		</Routes>
 	);
