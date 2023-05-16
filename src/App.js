849627394412c61e52917a69b7a63713e9055e8c
Reese <arter.charise@gmail.com>
2023-05-16 10:55:19 -0700

    added firebase + set up sign-in page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 import { Routes, Route } from 'react-router-dom';
 
 const Shop = () => {
@@ -14,6 +15,7 @@ const App = () => {
 				 */}
 				<Route index element={<Home />} />
 				<Route path='/shop' element={<Shop />} />
+				<Route path='/sign-in' element={<SignIn />} />
 			</Route>
 		</Routes>
 	);
