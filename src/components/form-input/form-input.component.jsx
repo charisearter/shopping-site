348cc4ff9e8c32add0a592874b73a590c0a7ff10
Reese <arter.charise@gmail.com>
2023-05-23 17:20:47 -0700

    added styles to form input component

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import './form-input.styles.scss';
 const FormInput = ({ label, ...otherProps }) => {
 	return (
-		<div>
-			<label>{label}</label>
-			<input {...otherProps} />
+		<div className='group'>
+			<input className='form-input' {...otherProps} />
+			{label && (
+				<label
+					className={`${
+						otherProps.value.length ? 'shrink' : null
+					} form-input-label`}
+				>
+					{label}
+				</label>
+			)}
 		</div>
 	);
 };
