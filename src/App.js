a30bf0ee5f4437dd728257c626d8fc0458fcdf12
Reese <arter.charise@gmail.com>
2023-05-26 12:47:46 -0700

    renamed signIn to auth, fixed links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in.component';
+import Authentication from './routes/authentication/authentication.component';
 import { Routes, Route } from 'react-router-dom';
 
 const Shop = () => {
@@ -15,7 +15,7 @@ const App = () => {
 				 */}
 				<Route index element={<Home />} />
 				<Route path='/shop' element={<Shop />} />
-				<Route path='/signIn' element={<SignIn />} />
+				<Route path='/auth' element={<Authentication />} />
 			</Route>
 		</Routes>
 	);
