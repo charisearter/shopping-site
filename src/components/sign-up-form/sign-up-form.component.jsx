8631e4d9e08fab8f87af48b1133721c233e67840
Reese <arter.charise@gmail.com>
2023-05-26 12:39:20 -0700

    created button component and styles

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
 import {
 	createAuthUserWithEmailAndPassword,
 	createUserDocumentFromAuth,
@@ -98,7 +99,7 @@ function SignUpForm() {
 					name='confirmPassword'
 					value={confirmPassword}
 				/>
-				<button type='submit'>Sign Up</button>
+				<Button type='submit'>Sign Up</Button>
 			</form>
 		</div>
 	);
