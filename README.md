cc813db5296c5982b20308f57f77ec4584e471c0
Reese <arter.charise@gmail.com>
2023-05-31 22:25:43 -0700

    cart functionality complete, updated readME

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
 ## Need to do
+
 [x] Basic Set up
-  [x] Home page
-  [x] Navigation
-  [x] Shop
-  [x] SignIN/Out
-  [x] Sass
+[x] Home page
+[x] Navigation
+[x] Shop
+[x] SignIN/Out
+[x] Sass
 
 [x] Basic Setup of Firebase Authentication
-  [x] Email password login
-  [x] Google Log in
+[x] Email password login
+[x] Google Log in
 
-[ ] Cart
-  [x] Basic component
-  [ ] Cart Items
-  [ ] Stripe
+[ ] Cart Functionality
+[x] Basic component
+[x] Cart Items
+[x] Add Item
+[x] Remove Item
+[x] Clear From Cart
+[x] Update Prices
+[x] Total
 
-[ ] FirebaseDB SetUp
+[] FirebaseDB SetUp - dynamic calls to DB
 
+[] Stripe
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -5,9 +5,7 @@ import './checkout.styles.scss';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 const Checkout = () => {
-	const { cartItems, addItemToCart, removeItemFromCart, clearItemFromCart } = useContext(
-		CartContext
-	);
+	const { cartItems, cartTotal } = useContext(CartContext);
 
 	return (
 		<div className='checkout-container'>
@@ -31,7 +29,7 @@ const Checkout = () => {
 			{cartItems.map((cartItem) => {
 				return <CheckoutItem key={cartItem.id} cartItem={cartItem} />;
 			})}
-			<span className='total'>Total: 0</span>
+			<span className='total'>Total: ${cartTotal}</span>
 		</div>
 	);
 };
